e05291549d20b564d64fbbda51423f97693c56d4
osamaIqbal6 <157040489+osamaIqbal6@users.noreply.github.com>
2024-01-20 02:27:42 -0800

    completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,17 +2,17 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={1.95} groundColor="white" />
       <pointLight intensity={5} />
-      <spotLight position={[-20, 50, 10]} intensity={5}/>
+      <spotLight position={[-20, 50, 10]} intensity={5} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.75, -1.5]}
+        scale={isMobile ? 0.1 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -20,6 +20,19 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setisMobile] = useState(false);
+  useEffect(() => {
+    const media = window.matchMedia("(max-width : 500px)");
+    setisMobile(media.matches);
+
+    const handlechange = (event) => {
+      setisMobile(event.matches);
+    };
+
+    media.addEventListener("change", handlechange);
+
+    return () => media.removeEventListener("change", handlechange);
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -33,7 +46,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />;
     </Canvas>
